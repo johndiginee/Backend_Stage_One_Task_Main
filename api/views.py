0620bfa787c5a9691a3ea9d4b87915d9e12670ed
John Diginee <Johndbizz@gmail.com>
2023-09-07 14:17:09 +0100

    Chore: Fix bug

--- api/views.py
+++ api/views.py
@@ -3,16 +3,16 @@ import datetime
 import json
 
 def list(request):
-    #the get query from url
+    # the get query from url
     slack_name = request.GET.get("slack_name")
     track = request.GET.get("track")
 
-    #to confirm that the query are provided
+    # Validate input
     if not slack_name or not track:
         return JsonResponse({"error":"Both slack name and track must be provided"}, status=400)
     
     github_repo = "https://github.com/johndiginee/Backend_Stage_One_Task_Main"
-    github_file = "f'{github.com/johndiginee}/blob/master/api/views.py"
+    github_file = "https://github.com/johndiginee/Backend_Stage_One_Task_Main/blob/master/api/views.py"
     cur_day = datetime.datetime.utcnow().strftime('%A')
     utctime = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
 
